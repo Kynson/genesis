c4248ea3d0f0d39c12fdba05c703e851922325df
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2026-01-26 15:49:07 +0800

    Bug fix for coredns config

--- services/coredns.nix
+++ services/coredns.nix
@@ -2,12 +2,12 @@
   services.coredns = {
     enable = true;
     config = ''
-      template IN A .local {
-        match ^(?P<d>[0-9]*)[.](?P<c>[0-9]*)[.](?P<b>[0-9]*)[.](?P<a>[0-9]*)[.]local$
-        answer "{{ .Name }} 60 IN A {{ .Group.a }}.{{ .Group.b }}.{{ .Group.c }}.{{ .Group.d }}"
-      }
-
       . {
+        template IN A .local {
+          match ^(?P<d>[0-9]*)[.](?P<c>[0-9]*)[.](?P<b>[0-9]*)[.](?P<a>[0-9]*)[.]local$
+          answer "{{ .Name }} 60 IN A {{ .Group.a }}.{{ .Group.b }}.{{ .Group.c }}.{{ .Group.d }}"
+        }
+
         forward . 1.1.1.1 1.0.0.1 8.8.8.8
       }
     '';
