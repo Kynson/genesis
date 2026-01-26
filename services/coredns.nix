ef3429556a961e912e0ca311faa0790425d0a61b
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2026-01-26 18:18:47 +0800

    Update coredns configs

--- services/coredns.nix
+++ services/coredns.nix
@@ -3,13 +3,21 @@
     enable = true;
     config = ''
       . {
-        template IN A local {
-          match ^(?P<d>[0-9]*)[.](?P<c>[0-9]*)[.](?P<b>[0-9]*)[.](?P<a>[0-9]*)[.]local$
+        health 0.0.0.0:5353
+
+        template IN A 0x4b5354.dev {
+          match ^(.+[.])?(?P<a>[0-9]{1,3})[.](?P<b>[0-9]{1,3})[.](?P<c>[0-9]{1,3})[.](?P<d>[0-9]{1,3})[.]0x4b5354[.]dev[.]$
           answer "{{ .Name }} 60 IN A {{ .Group.a }}.{{ .Group.b }}.{{ .Group.c }}.{{ .Group.d }}"
+          fallthrough
         }
 
         forward . 1.1.1.1 1.0.0.1 8.8.8.8
       }
     '';
   };
+
+  # Security: This server is deployed without port forwarding and is not accessible from the internet.
+  # This will only allow DNS queries and health checks from local network.
+  networking.firewall.allowedUDPPorts = [ 53 ];
+  networking.firewall.allowedTCPPorts = [ 5353 ];
 }
