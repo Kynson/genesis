55c7de3d351e27e8e9a0d9346fc388d0fdf6b756
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2026-01-26 15:55:59 +0800

    Bug fix for coredns config

--- services/coredns.nix
+++ services/coredns.nix
@@ -3,7 +3,7 @@
     enable = true;
     config = ''
       . {
-        template IN A .local {
+        template IN A local {
           match ^(?P<d>[0-9]*)[.](?P<c>[0-9]*)[.](?P<b>[0-9]*)[.](?P<a>[0-9]*)[.]local$
           answer "{{ .Name }} 60 IN A {{ .Group.a }}.{{ .Group.b }}.{{ .Group.c }}.{{ .Group.d }}"
         }
